2ae5590cdc9cae9a419b0f94c42af12f8d821c57
Shakil Ops <shakildo.7887@gmail.com>
2023-07-12 14:27:41 +0600

    [Done] improve lint setup, create interface,model,route,controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -8,9 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//Application routes
+app.use('/api/v1/users/', usersRouter)
+
 //testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working successfully')
 })
 
 export default app
