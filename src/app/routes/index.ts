f2a2c614f149b95cb114c48ac19f7dbfbbc7fa4c
Shakil Ops <shakildo.7887@gmail.com>
2025-01-20 17:34:43 +0600

    semester student faculty done

---
+++ src/app/routes/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+
+import { academicDepartmentRoutes } from "../modules/academicDepartment.ts/academicDepartment.routes";
+import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
+import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.route";
+import { UserRoutes } from "../modules/user/user.route";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: UserRoutes,
+  },
+  {
+    path: "/academic-semesters",
+    route: AcademicSemesterRoutes,
+  },
+  {
+    path: "/academic-faculties",
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: "/academic-departments",
+    route: academicDepartmentRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
